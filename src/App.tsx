77c7c9d5cc0d6c785335c984399d29820f46b5a7
John Roice Aldeza <jroicealdeza@gmail.com>
2025-02-07 15:57:37 +0800

    Add routing for checklist and add check pages with main layout integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import ProtectedRoute from "./layouts/ProtectedRoute";
+import AddCheckPage from "./pages/AddCheckPage";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import MainLayout from "./layouts/MainLayout";
+import ChecklistPage from "./pages/checklist/ChecklistPage";
 
 function App() {
   // Check if the user is authenticated and redirect to the login page if not
-
 
   return (
     <main>
@@ -16,11 +18,49 @@ function App() {
             path="/"
             element={
               <ProtectedRoute>
-                <Home />
+                <MainLayout>
+                  <Home />
+                </MainLayout>
               </ProtectedRoute>
             }
           />
           <Route path="/login" element={<Login />} />
+          <Route
+            path="/check/add"
+            element={
+              <ProtectedRoute>
+                <MainLayout>
+                  <AddCheckPage />
+                </MainLayout>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/checklist/:id"
+            element={
+              <ProtectedRoute>
+                <MainLayout>
+                  <ChecklistPage />
+                </MainLayout>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <ProtectedRoute>
+                <MainLayout>
+                  <h1>
+                    Not Found. Go to
+                    <a href="/" className="text-blue-500 ">
+                      {" "}
+                      Home.
+                    </a>
+                  </h1>
+                </MainLayout>
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </main>
