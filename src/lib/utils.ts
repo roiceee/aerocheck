4b1dd98f4a86e8f5db22bdd332b6d2719f267c05
John Roice Aldeza <jroicealdeza@gmail.com>
2025-02-07 15:58:11 +0800

    Add combineDateTime utility function for date and time formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function combineDateTime(dateString: string, timeString: string) {
+  if (!dateString || !timeString) return null;
+
+  dateString = dateString.split("T")[0];
+
+  return `${dateString}T${timeString}:00`; // Format: YYYY-MM-DDTHH:MM:SS
 }
