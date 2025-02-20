e99d9b77f87fea5fbf13b26cd2c0965940adc8c8
John Roice Aldeza <jroicealdeza@gmail.com>
2025-02-20 23:58:08 +0800

    Update Badge component to change warning text color to secondary foreground

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -16,7 +16,7 @@ const badgeVariants = cva(
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
         success: "border-transparent bg-green-500 text-primary-foreground",
-        warning: "border-transparent bg-yellow-500 text-primary-foreground",
+        warning: "border-transparent bg-yellow-500 text-secondary-foreground",
       },
     },
     defaultVariants: {
