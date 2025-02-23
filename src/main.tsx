535732fff3fbb5108b2d55cc793b95b4a5ace1f2
John Roice Aldeza <jroicealdeza@gmail.com>
2025-02-23 22:47:53 +0800

    Add next-themes and sonner dependencies to package.json and package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContextProvider.tsx";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+      <AuthProvider>
+        <App />
+        <Toaster />
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
 );
