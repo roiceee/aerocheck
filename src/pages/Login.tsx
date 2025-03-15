22078fcd0522fdee0503c5cc572fa736efbfc0e9
John Roice Aldeza <jroicealdeza@gmail.com>
2025-03-15 18:10:32 +0800

    Remove custom shape divider from Login component and clean up CSS

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,19 +20,6 @@ export default function Login() {
           style={{ backgroundImage: "url('/bg.jpg')" }}
         >
           <div className="absolute inset-0 bg-black opacity-10" />
-          <div className="custom-shape-divider-bottom-1742032009 lg:hidden">
-            <svg
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 1200 120"
-              preserveAspectRatio="none"
-            >
-              <path
-                d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z"
-                className="shape-fill"
-              ></path>
-            </svg>
-          </div>
         </div>
 
         {/* Login Content */}
